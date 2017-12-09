c31378f555d00392b5e0a604cf22018f599fb805
admin <admin@admin-PC>
2017-12-09 17:37:24 +0800

    修改配置

--- sourcecode/breeze/rain/rain-web/src/main/java/git/oschina/net/justlive1/breeze/rain/web/controller/HelloController.java
+++ sourcecode/breeze/rain/rain-web/src/main/java/git/oschina/net/justlive1/breeze/rain/web/controller/HelloController.java
@@ -1,20 +1,20 @@
 package git.oschina.net.justlive1.breeze.rain.web.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import git.oschina.net.justlive1.breeze.rain.api.service.HelloService;
 
-@Controller
+@RestController
 @RequestMapping("/test")
 public class HelloController {
 
 	@Autowired
 	HelloService helloService;
 
-	@GetMapping
+	@GetMapping("/hello")
 	public String hello() {
 
 		return helloService.say();
--- sourcecode/breeze/rain/rain-web/src/main/java/git/oschina/net/justlive1/breeze/rain/web/service/HelloServiceImpl.java
+++ sourcecode/breeze/rain/rain-web/src/main/java/git/oschina/net/justlive1/breeze/rain/web/service/HelloServiceImpl.java
@@ -1,16 +1,19 @@
 package git.oschina.net.justlive1.breeze.rain.web.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import git.oschina.net.justlive1.breeze.rain.api.service.HelloService;
 
 @Service
-public class HelloServiceImpl implements HelloService{
+public class HelloServiceImpl implements HelloService {
 
-	
+	@Autowired
+	RestTemplate template;
+
 	@Override
 	public String say() {
-		// TODO Auto-generated method stub
-		return null;
+		return template.getForObject("http://127.0.0.1:8080/rain-service/test/hello", String.class);
 	}
 }
