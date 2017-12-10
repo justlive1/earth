e884a795b62e516a998a7e128ea20faf139128b8
justlive1 <qq11419041@163.com>
2017-12-10 23:20:16 +0800

    配置

--- sourcecode/breeze/rain/rain-web/src/main/java/git/oschina/net/justlive1/breeze/rain/web/service/HelloServiceImpl.java
+++ sourcecode/breeze/rain/rain-web/src/main/java/git/oschina/net/justlive1/breeze/rain/web/service/HelloServiceImpl.java
@@ -1,6 +1,7 @@
 package git.oschina.net.justlive1.breeze.rain.web.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -12,8 +13,11 @@ public class HelloServiceImpl implements HelloService {
 	@Autowired
 	RestTemplate template;
 
+	@Value("${rain.service.url}")
+	String serviceUrl;
+
 	@Override
 	public String say() {
-		return template.getForObject("http://127.0.0.1:8080/rain-service/test/hello", String.class);
+		return template.getForObject(serviceUrl + "/test/hello", String.class);
 	}
 }
