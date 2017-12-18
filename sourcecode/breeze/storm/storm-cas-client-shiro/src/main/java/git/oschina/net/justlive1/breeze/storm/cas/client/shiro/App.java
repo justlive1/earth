1167b7c38981eb9511f93950918a1c5c94dd1b56
justlive1 <qq11419041@163.com>
2017-12-18 21:34:37 +0800

    初始化storm-cas-client-shiro

---
+++ sourcecode/breeze/storm/storm-cas-client-shiro/src/main/java/git/oschina/net/justlive1/breeze/storm/cas/client/shiro/App.java
@@ -0,0 +1,11 @@
+package git.oschina.net.justlive1.breeze.storm.cas.client.shiro;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
