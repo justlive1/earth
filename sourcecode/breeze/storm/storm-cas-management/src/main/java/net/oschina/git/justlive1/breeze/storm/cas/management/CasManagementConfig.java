103ef390db270925b89c72bd9902ff2ae8ee2b66
justlive1 <qq11419041@163.com>
2018-01-15 23:51:28 +0800

    cas-management

---
+++ sourcecode/breeze/storm/storm-cas-management/src/main/java/net/oschina/git/justlive1/breeze/storm/cas/management/CasManagementConfig.java
@@ -0,0 +1,16 @@
+package net.oschina.git.justlive1.breeze.storm.cas.management;
+
+import org.springframework.context.annotation.Configuration;
+
+import net.oschina.git.justlive1.breeze.snow.common.base.util.AbstractTrustAllManager;
+
+/**
+ * cas扩展配置类
+ * 
+ * @author wubo
+ *
+ */
+@Configuration("stormCasManagementConfig")
+public class CasManagementConfig extends AbstractTrustAllManager {
+
+}
