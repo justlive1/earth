e2c4e654f29552c8462ce37084db5b7544b51329
aofenghecanyue <aofenghecanyue@163.com>
2018-01-15 21:17:01 +0800

    shiro配置类

--- sourcecode/breeze/storm/storm-cas-client-shiro/src/main/java/net/oschina/git/justlive1/breeze/storm/cas/client/shiro/web/ShiroConfiguration.java
+++ sourcecode/breeze/storm/storm-cas-client-shiro/src/main/java/net/oschina/git/justlive1/breeze/storm/cas/client/shiro/web/ShiroConfiguration.java
@@ -3,10 +3,12 @@
  */
 package net.oschina.git.justlive1.breeze.storm.cas.client.shiro.web;
 
+import org.apache.shiro.spring.web.config.AbstractShiroWebFilterConfiguration;
+
 /**
  * @author liuwenlai
  *
  */
-public class ShiroConfiguration {
+public class ShiroConfiguration extends AbstractShiroWebFilterConfiguration{
 
 }
