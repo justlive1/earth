13b1292150c1a478c03af5d3e7747b4ff1be7b7c
justlive1 <qq11419041@163.com>
2018-01-28 23:31:54 +0800

    spring boot admin server

---
+++ sourcecode/breeze/cloud/cloud-admin-server/src/main/java/net/oschina/git/justlive1/breeze/cloud/admin/server/SpringBootAdminApplication.java
@@ -0,0 +1,23 @@
+package net.oschina.git.justlive1.breeze.cloud.admin.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+
+import de.codecentric.boot.admin.config.EnableAdminServer;
+
+/**
+ * 程序入口
+ * 
+ * @author wubo
+ *
+ */
+@Configuration
+@EnableAutoConfiguration
+@EnableAdminServer
+public class SpringBootAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAdminApplication.class, args);
+	}
+}
