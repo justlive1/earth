3164010cb8c7b6c2ba979bf2ec4e5281f4c4ff95
justlive1 <qq11419041@163.com>
2018-01-29 21:37:20 +0800

    spring boot admin client

---
+++ sourcecode/breeze/cloud/cloud-admin-client/src/main/java/net/oschina/git/justlive1/breeze/cloud/admin/client/App.java
@@ -0,0 +1,12 @@
+package net.oschina.git.justlive1.breeze.cloud.admin.client;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
