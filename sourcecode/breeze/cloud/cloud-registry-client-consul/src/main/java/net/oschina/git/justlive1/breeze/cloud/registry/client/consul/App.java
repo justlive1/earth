3f2211d0dd6c1aa0135f58b60eadb694a277df7f
justlive1 <qq11419041@163.com>
2018-02-08 19:47:07 +0800

    consul服务注册client

---
+++ sourcecode/breeze/cloud/cloud-registry-client-consul/src/main/java/net/oschina/git/justlive1/breeze/cloud/registry/client/consul/App.java
@@ -0,0 +1,13 @@
+package net.oschina.git.justlive1.breeze.cloud.registry.client.consul;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
