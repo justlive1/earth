0740e11db1e6ab1fefbe02e84efa61004e27d655
justlive1 <qq11419041@163.com>
2018-02-13 23:24:29 +0800

    增加admin-server-zookeeper

---
+++ sourcecode/breeze/cloud/cloud-admin-server-zookeeper/src/test/java/net/oschina/git/justlive1/breeze/cloud/admin/server/zookeeper/SpringBootAdminApplicationTest.java
@@ -0,0 +1,34 @@
+package net.oschina.git.justlive1.breeze.cloud.admin.server.zookeeper;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class SpringBootAdminApplicationTest extends TestCase {
+    /**
+     * Create the test case
+     *
+     * @param testName
+     *            name of the test case
+     */
+    public SpringBootAdminApplicationTest(String testName) {
+        super(testName);
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite() {
+        return new TestSuite(SpringBootAdminApplicationTest.class);
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp() {
+        assertTrue(true);
+    }
+}
