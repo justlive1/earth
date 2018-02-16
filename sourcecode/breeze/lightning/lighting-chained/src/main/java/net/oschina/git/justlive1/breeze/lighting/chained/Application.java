ff2fd18ca3d5419b724eb96bfae4973fa0d78ea3
justlive1 <qq11419041@163.com>
2018-02-16 21:25:07 +0800

    增加lighting-chained

---
+++ sourcecode/breeze/lightning/lighting-chained/src/main/java/net/oschina/git/justlive1/breeze/lighting/chained/Application.java
@@ -0,0 +1,19 @@
+package net.oschina.git.justlive1.breeze.lighting.chained;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 程序入口
+ * 
+ * @author wubo
+ *
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(Application.class, args);
+    }
+}
