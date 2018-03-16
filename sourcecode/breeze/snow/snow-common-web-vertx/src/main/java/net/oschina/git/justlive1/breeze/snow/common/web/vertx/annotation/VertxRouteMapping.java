374da0402d9d203c0311fbb1a0649b7a76accdd8
justlive1 <qq11419041@163.com>
2018-03-16 22:52:10 +0800

    增加common-web-vertx

---
+++ sourcecode/breeze/snow/snow-common-web-vertx/src/main/java/net/oschina/git/justlive1/breeze/snow/common/web/vertx/annotation/VertxRouteMapping.java
@@ -0,0 +1,60 @@
+package net.oschina.git.justlive1.breeze.snow.common.web.vertx.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import io.vertx.core.http.HttpMethod;
+
+/**
+ * route的请求映射
+ * 
+ * @author wubo
+ *
+ */
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface VertxRouteMapping {
+  /**
+   * 地址映射 (e.g. "/myPath.do").
+   * 
+   * @return
+   */
+  String[] value() default {};
+
+  /**
+   * HTTP请求类型 :GET, POST, HEAD, OPTIONS, PUT, PATCH, DELETE, TRACE.
+   * 
+   * @return
+   */
+  HttpMethod[] method() default {};
+
+  /**
+   * 指定处理请求的提交内容类型(Content-Type) *
+   * 
+   * <pre class="code">
+   * consumes = "text/plain"
+   * consumes = {"text/plain", "application/*"}
+   * </pre>
+   * 
+   * @return
+   */
+  String[] consumes() default {};
+
+  /**
+   * 指定返回的内容类型，仅当request请求头中的(Accept)类型中包含该指定类型才返回
+   * 
+   * 
+   * <pre class="code">
+   * produces = "text/plain"
+   * produces = {"text/plain", "application/*"}
+   * produces = "application/json; charset=UTF-8"
+   * </pre>
+   * 
+   * @return
+   */
+  String[] produces() default {};
+
+}
